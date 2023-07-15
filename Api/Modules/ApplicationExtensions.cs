6939e2f27b6ea986c6bb21564fa4774f56505793
Victor Prouff <contact@victorprouff.fr>
2023-07-15 17:17:11 +0200

    Fix

--- Api/Modules/ApplicationExtensions.cs
+++ Api/Modules/ApplicationExtensions.cs
@@ -33,16 +33,16 @@ public static class ApplicationExtensions
                 c.GetRequiredService<INuggetRepository>(),
                 c.GetRequiredService<IUserRepository>(),
                 c.GetRequiredService<IFileStorage>(),
-                configuration.GetValue<string>("CleverCloud:Host")));
+                configuration.GetValue<string>("CELLAR_ADDON_HOST")));
         
         services.AddTransient<IUserDomain, UserDomain>();
 
         services.AddSingleton<IAmazonS3, AmazonS3Client>(_ => new AmazonS3Client(
-            configuration.GetValue<string>("CleverCloud:AccessKeyId"),
-            configuration.GetValue<string>("CleverCloud:SecretAccessKey"),
+            configuration.GetValue<string>("CELLAR_ADDON_KEY_ID"),
+            configuration.GetValue<string>("CELLAR_ADDON_KEY_SECRET"),
             new AmazonS3Config
             {
-                ServiceURL = configuration.GetValue<string>("CleverCloud:Url")
+                ServiceURL = configuration.GetValue<string>("CELLAR_ADDON_URL")
             }
         ));
 
